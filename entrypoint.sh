1faf4997043ac956a6f93e99d5a1681627d11337
ZhengX <56376387+Megrax@users.noreply.github.com>
2021-05-13 18:14:32 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-hugo --gc --minify --cleanDestinationDir
+hugo -D
 
 if [ -z "$INPUT_SECRET_ID" ]; then
   echo '::error::Required SecretId parameter'
@@ -24,7 +24,12 @@ if [ -z "$INPUT_REGION" ]; then
   exit 1
 fi
 
-coscmd config -a $INPUT_SECRET_ID -s $INPUT_SECRET_KEY -b $INPUT_BUCKET -r $INPUT_REGION -m 30
+if [ -z "$INPUT_ENDPOINT" ]; then
+  echo '::error::Required Region parameter'
+  exit 1
+fi
+
+coscmd config -a $INPUT_SECRET_ID -s $INPUT_SECRET_KEY -b $INPUT_BUCKET -r $INPUT_REGION -e $INPUT_ENDPOINT -m 30
 coscmd upload -rs --delete -f public/ /
 
 echo "COS deploy successfully"
